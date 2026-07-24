2dc849d754366ea908f863f458178786da4c2f1a
XiaoFeng9009 <xiaofenglu894@gmail.com>
2026-07-24 10:47:36 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-﻿import { useState } from 'react'
+﻿import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import BackToTop from './BackToTop'
 import useBreakpoint from '../lib/useBreakpoint'
